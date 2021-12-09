48cb02d1e310ba5260c87e5714a0694d99787abe
Bram <bram.boogaarts@gmail.com>
2021-12-09 16:08:04 +0100

    Standaardisering kolomnamen en indexes + requirements update

--- Verkiezingen_functies.py
+++ Verkiezingen_functies.py
@@ -14,34 +14,34 @@ def landelijke_uitslag(uitslagenDF):
     kiesdeler = int(totaal_stemmen / aantal_zetels + 0.5)
     
     
-    zetelsDF = pd.DataFrame(columns=['totaal aantal stemmen', 'aantal zetels'],
+    zetelsDF = pd.DataFrame(columns=['stemmen', 'zetels'],
                             index=uitslagenDF.columns[10:])
     
     # Bereken het aantal stemmen en volledige zetels voor elke partij
     for partij in uitslagenDF.columns[10:]:
         partij_stemmen = int(uitslagenDF[partij].sum())
-        zetelsDF.loc[partij, 'totaal aantal stemmen'] = partij_stemmen
+        zetelsDF.loc[partij, 'stemmen'] = partij_stemmen
         
         aantal_volle_zetels = int(partij_stemmen/kiesdeler)
-        zetelsDF.loc[partij, 'aantal zetels'] = aantal_volle_zetels
+        zetelsDF.loc[partij, 'zetels'] = aantal_volle_zetels
      
     # Haal de partijen die niet minstens 1 volledige zetel hebben gehaald uit het primaire DF,
     # zodat ze niet meegaan in de berekening van de restzetels. Sla ze op in hun eigen DF.
-    idx_partijen_zonder_zetels = zetelsDF[zetelsDF['aantal zetels']==0].index
+    idx_partijen_zonder_zetels = zetelsDF[zetelsDF['zetels']==0].index
     partijen_zonder_zetels = zetelsDF.loc[idx_partijen_zonder_zetels]
     zetelsDF = zetelsDF.drop(idx_partijen_zonder_zetels)
         
     zetelsDF.insert(2, 'stemmen per zetel', [0]*len(zetelsDF.index))
     
     # Toekenning restzetels
-    restzetels = 150 - zetelsDF['aantal zetels'].sum()
-    zetelsDF['stemmen per zetel'] = zetelsDF['totaal aantal stemmen'] / (zetelsDF['aantal zetels'] + 1)
+    restzetels = 150 - zetelsDF['zetels'].sum()
+    zetelsDF['stemmen per zetel'] = zetelsDF['stemmen'] / (zetelsDF['zetels'] + 1)
     zetelsDF['stemmen per zetel'] = pd.to_numeric(zetelsDF['stemmen per zetel'])
     
     while restzetels > 0:
         i_max = zetelsDF['stemmen per zetel'].idxmax() # index (partijnaam) van hoogste aantal stemmen per zetel
-        zetelsDF.loc[i_max, 'aantal zetels'] += 1
-        zetelsDF.loc[i_max, 'stemmen per zetel'] = zetelsDF.loc[i_max, 'totaal aantal stemmen'] / (zetelsDF.loc[i_max, 'aantal zetels'] + 1)
+        zetelsDF.loc[i_max, 'zetels'] += 1
+        zetelsDF.loc[i_max, 'stemmen per zetel'] = zetelsDF.loc[i_max, 'stemmen'] / (zetelsDF.loc[i_max, 'zetels'] + 1)
         restzetels -= 1
     
     zetelsDF = zetelsDF.drop('stemmen per zetel', axis=1)
@@ -68,12 +68,12 @@ def volgorde_gemeentes(uitslagenDF, partij):
         return "De partijnaam wordt niet herkend!"
     
     else:        
-        partijDF = pd.DataFrame(data=list(uitslagenDF[partijnaam]), columns=['aantal stemmen'],
+        partijDF = pd.DataFrame(data=list(uitslagenDF[partijnaam]), columns=['stemmen'],
                                 index=list(uitslagenDF['RegioNaam']))
         
         # Sorteer op aantal stemmen, waar NaN wordt vervangen door 0.
-        sorted_partijDF = partijDF.sort_values('aantal stemmen', ascending=False,
-                                               key=replace_NaN(partijDF['aantal stemmen']))
+        sorted_partijDF = partijDF.sort_values('stemmen', ascending=False,
+                                               key=replace_NaN(partijDF['stemmen']))
         # Maak alle entries van type int (in het geval het nog floats zijn).
         sorted_partijDF = sorted_partijDF.astype(int)
         
@@ -123,11 +123,11 @@ def uitslag_gemeente(uitslagenDF, gemeente):
         
     gemeente_idx = uitslagenDF[uitslagenDF['RegioNaam'] == gemeente].index[0]
     gemeenteDF = pd.DataFrame(data=list(uitslagenDF.loc[gemeente_idx, uitslagenDF.columns[10:]]),
-                              columns=['aantal stemmen'], index=list(uitslagenDF.columns[10:]))
+                              columns=['stemmen'], index=list(uitslagenDF.columns[10:]))
         
     # Sorteer op aantal stemmen, waar NaN wordt vervangen door 0.
-    sorted_gemeenteDF = gemeenteDF.sort_values('aantal stemmen', ascending=False,
-                                               key=replace_NaN(gemeenteDF['aantal stemmen']))
+    sorted_gemeenteDF = gemeenteDF.sort_values('stemmen', ascending=False,
+                                               key=replace_NaN(gemeenteDF['stemmen']))
     # Maak alle entries van type int (in het geval het nog floats zijn).
     sorted_gemeenteDF = sorted_gemeenteDF.astype(int)
         
@@ -157,20 +157,21 @@ def zetels_per_gewonnen_gemeente(df):
     gemeente_winnaars = populairste_per_gemeente(df)
     gemeente_count = len(gemeente_winnaars.index)
     winnaar_count = gemeente_winnaars.groupby(['Winnaar']).count()
-    zetels = pd.DataFrame(columns=['Fractie','Zetels'])
+    zetels = pd.DataFrame(data=0, columns=['zetels', 'winst_per_zetel', 'totaal_winst'], index=df.columns[10:])
     zetel_per_winst = 150/gemeente_count
 
     for index,row in winnaar_count.iterrows():
         if round(row[0] * zetel_per_winst) > 1:
             volle_zetels = round(row[0] * zetel_per_winst)
-            zetels = zetels.append({'Fractie': index,'Zetels': volle_zetels, 'winst_per_zetel': (volle_zetels+1)/row[0], 'totaal_winst': row[0] },ignore_index=True)
+            zetels.loc[index] = [volle_zetels, row[0]/(volle_zetels+1), row[0]]
     
-    rest_zetels = 150 - zetels['Zetels'].sum()
+    rest_zetels = 150 - zetels['zetels'].sum()
+    zetels['winst_per_zetel'] = pd.to_numeric(zetels['winst_per_zetel'])
 
     while rest_zetels > 0:
-        i_max = zetels['winst_per_zetel'].idxmax() # index (partijnaam) van hoogste aantal stemmen per zetel
-        zetels.loc[i_max, 'Zetels'] += 1
-        zetels.loc[i_max, 'winst_per_zetel'] = zetels.loc[i_max, 'totaal_winst'] / (zetels.loc[i_max, 'Zetels'] + 1)
+        i_max = zetels['winst_per_zetel'].idxmax() # index (partijnaam) van hoogste aantal gewonnen gemeenten per zetel
+        zetels.loc[i_max, 'zetels'] += 1
+        zetels.loc[i_max, 'winst_per_zetel'] = zetels.loc[i_max, 'totaal_winst'] / (zetels.loc[i_max, 'zetels'] + 1)
         rest_zetels -= 1
 
     zetels = zetels.drop(columns=['totaal_winst','winst_per_zetel'])
@@ -205,7 +206,7 @@ def landelijke_uitslag_kiesmannen(uitslagenDF):
     
     
     # Bepalen van de winnaar in elke gemeente en het toekennnen van de kiesmannen
-    zetelsDF = pd.DataFrame(data=0, columns=['Aantal zetels'], index=uitslagenDF.columns[10:])
+    zetelsDF = pd.DataFrame(data=0, columns=['zetels'], index=uitslagenDF.columns[10:])
     zetelsDF.index.name = 'Partijnaam'
     
     for i in uitslagenDF.index:
@@ -214,9 +215,11 @@ def landelijke_uitslag_kiesmannen(uitslagenDF):
         temp_ser = temp_ser.astype(int)
         winnaar = temp_ser.idxmax()
         gemeente = uitslagenDF.loc[i, 'RegioNaam']
-        zetelsDF.loc[winnaar, 'Aantal zetels'] += kiesmannenDF.loc[gemeente, 'Kiesmannen']
+        zetelsDF.loc[winnaar, 'zetels'] += kiesmannenDF.loc[gemeente, 'Kiesmannen']
         
     return zetelsDF
+
+
 
 def stem_stad_n(df, stad='Amsterdam', n=3):
     """
@@ -278,26 +281,26 @@ def landelijke_uitslag_top_n(df, n=3):
             else:
                 landelijk[partij] = zetel
                 
-    data = {"partij": landelijk.keys(), "zetels": landelijk.values()}
-    my_df = pd.DataFrame(data).sort_values(["zetels"], ascending=False)
+    my_df = pd.DataFrame(data=landelijk.values(), columns=['zetels'], index=landelijk.keys()).sort_values(["zetels"], ascending=False)
     my_df = my_df.drop(my_df[my_df['zetels'] < 1].index)
     my_df = my_df.astype({'zetels': int})
 
     # restzetels
-    my_df.insert(2, "stemmen per zetel", [0]*len(my_df.index))
-    my_df.insert(3, "aantal stemmen", [0]*len(my_df.index))
+    my_df.insert(1, "stemmen per zetel", [0]*len(my_df.index))
+    my_df.insert(2, "stemmen", [0]*len(my_df.index))
 
-    for i in range(0, len(my_df.index)):
-        index = my_df.index[i]
-        partij = my_df["partij"].loc[index]
+    for partij in my_df.index:
         aantal_stemmen = df[partij].sum()
-        my_df.loc[index, 'aantal stemmen'] = aantal_stemmen
-        my_df.loc[index, 'stemmen per zetel'] = aantal_stemmen / (my_df["zetels"][index] +1)              
+        my_df.loc[partij, 'stemmen'] = aantal_stemmen
+        my_df.loc[partij, 'stemmen per zetel'] = aantal_stemmen / (my_df["zetels"][partij] +1)              
 
     while my_df['zetels'].sum() < 150:
         i_max = my_df['stemmen per zetel'].idxmax()
         my_df.loc[i_max, 'zetels'] += 1
-        my_df.loc[i_max, 'stemmen per zetel'] = my_df.loc[i_max, 'aantal stemmen'] / (my_df.loc[i_max, 'zetels'] +1)
+        my_df.loc[i_max, 'stemmen per zetel'] = my_df.loc[i_max, 'stemmen'] / (my_df.loc[i_max, 'zetels'] +1)
+        
+    my_df = my_df.drop('stemmen per zetel', axis=1)
+        
     return my_df.sort_values(["zetels"], ascending=False)
 
 
