02307b1b7839090186fca82a02e4d43b15336325
Bram <bram.boogaarts@gmail.com>
2021-12-09 09:48:15 +0100

    Standaardisering endpoints en outputs

--- app.py
+++ app.py
@@ -15,14 +15,14 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
-@app.route("/landelijke_uitslag/werkelijk/", methods=['GET'])
+@app.route("/landelijke_uitslag/werkelijk", methods=['GET'])
 def get_landelijke_uitslag():
-    return verfuncs.landelijke_uitslag(uitslagenDF)
+    return verfuncs.landelijke_uitslag(uitslagenDF).to_html()
 
 
-@app.route("/landelijke_uitslag/kiesmannen/", methods=['GET'])
+@app.route("/landelijke_uitslag/kiesmannen", methods=['GET'])
 def get_landelijke_uitslag_kiesmannen():
-    return verfuncs.landelijke_uitslag_kiesmannen(uitslagenDF)
+    return verfuncs.landelijke_uitslag_kiesmannen(uitslagenDF).to_html()
 
 @app.route('/landelijke_uitslag/top_n_partijen/<aantal>')
 def landelijk_top_n_partijen(aantal):
@@ -37,24 +37,28 @@ def landelijk_top_n_partijen(aantal):
 @app.route("/gemeente/uitslag/<gemeente>", methods=['GET'])
 def get_uitslag_gemeente(gemeente=""):
     if gemeente == "": return "Geef in de url aan van welke gemeente je de uitslag wil zien."
-    else: return verfuncs.uitslag_gemeente(uitslagenDF, gemeente)
+    elif gemeente in list(uitslagenDF['RegioNaam']):
+        return verfuncs.uitslag_gemeente(uitslagenDF, gemeente).to_html()
+    else: return "De gemeentenaam wordt niet herkend!", 400
 
 
 @app.route("/gemeente/geldig/", methods=['GET'])
 def get_volgorde_perc_ongeldig():
-    return verfuncs.volgorde_perc_ongeldig(uitslagenDF)
+    return verfuncs.volgorde_perc_ongeldig(uitslagenDF).to_html()
 
 
 @app.route("/gemeente/geldig/<gemeente>", methods=['GET'])
 def get_perc_ongeldig_gemeente(gemeente):
-    return verfuncs.perc_ongeldig_gemeente(uitslagenDF, gemeente)
+    if gemeente in list(uitslagenDF['RegioNaam']):
+        return verfuncs.perc_ongeldig_gemeente(uitslagenDF, gemeente).to_html()
+    else: return "De gemeentenaam wordt niet herkend!", 400
 
 
 @app.route("/partij/rangschikking/", methods=['GET'])
 @app.route("/partij/rangschikking/<partij>",methods=['GET'])
 def get_volgorde_gemeentes(partij=""):
     if partij == "": return "Geef in de url aan van welke partij je de rangschikking wil zien."
-    else: return verfuncs.volgorde_gemeentes(uitslagenDF, partij)
+    else: return verfuncs.volgorde_gemeentes(uitslagenDF, partij).to_html()
 
 
 @app.route("/alternatief/gemeente/winnaar")
